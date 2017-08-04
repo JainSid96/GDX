6e6b745d73ec89d5bcb97553db6b1c81bcdef50d
S:Ddhant Jain <jainsid123456789@gmail.com>
2017-08-04 17:27:46 +0530

    .

---
+++ Texture repo/core/src/com/jainsid/flappybird/FlappyBird.java
@@ -0,0 +1,42 @@
+package com.jainsid.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+
+	Texture[] birds;
+	int flapState = 0;
+	
+	@Override
+	public void create () {
+
+		batch = new SpriteBatch();
+		background = new Texture("bg.png");
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
+	}
+
+	@Override
+	public void render () {
+
+		if (flapState == 0){
+			flapState = 1;
+		}else {
+			flapState = 0;
+		}
+
+		batch.begin();
+		batch.draw(background , 0 , 0 , Gdx.graphics.getWidth() , Gdx.graphics.getHeight());
+		batch.draw(birds[flapState] , Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2 , Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2);
+		batch.end();
+
+	}
+
+}
