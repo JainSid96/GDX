9f9bb531ee1e029f0606233809aac70ef2f28522
S:Ddhant Jain <jainsid123456789@gmail.com>
2017-08-04 17:50:12 +0530

    .

---
+++ interactive sprites/core/src/com/jainsid/flappybird/FlappyBird.java
@@ -0,0 +1,68 @@
+package com.jainsid.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+
+	Texture[] birds;
+	int flapState = 0;
+	float birdY = 0;
+	float vilocity = 0;
+
+	int gameState = 0;
+	float gravity = 2;
+	
+	@Override
+	public void create () {
+
+		batch = new SpriteBatch();
+		background = new Texture("bg.png");
+
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight()/2 - birds[0].getHeight()/2;
+
+	}
+
+	@Override
+	public void render () {
+
+		if (gameState != 0) {
+
+			if (Gdx.input.justTouched()) {
+
+				vilocity = -30;
+			}
+			if (birdY > 0 || vilocity < 0){
+
+				vilocity = vilocity + gravity;
+				birdY -= vilocity;
+
+			}
+		}else{
+
+			if (Gdx.input.justTouched()) {
+				gameState = 1;
+			}
+		}
+
+		if (flapState == 0) {
+			flapState = 1;
+		} else {
+			flapState = 0;
+		}
+
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
+		batch.end();
+
+	}
+}
